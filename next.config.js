1485b2735d674a8e79218124e4ee1fa192b0fb3f
mqdesigncriativo-ship-it <mqdesigncriativo@gmail.com>
2025-10-14 15:38:44 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { experimental: { appDir: true } };
-module.exports = nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+}
+module.exports = nextConfig
